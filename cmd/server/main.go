bf67ef041b3416599eabe5a31e12d68b34d67d76
1f604 <1f604@users.noreply.github.com>
2022-03-19 21:05:18 +0000

    Add view handler

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -8,11 +8,22 @@ import (
 	"net/http"
 )
 
+var (
+	view_path = "/view/"
+	edit_path = "/edit/"
+)
+
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[:])
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len(view_path):]
+	fmt.Fprintf(w, "Hi there! I like %s!", title)
 }
 
 func main() {
+	http.HandleFunc(view_path, viewHandler)
 	http.HandleFunc("/", handler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
